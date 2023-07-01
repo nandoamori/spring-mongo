14fdd450db7cc371cd544bf0148f77de8863bd29
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2023-07-01 13:32:02 -0300

    Consulta simples com query methods

--- src/main/java/com/example/springMongo/services/PostService.java
+++ src/main/java/com/example/springMongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.example.springMongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,8 @@ public class PostService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 
 }
